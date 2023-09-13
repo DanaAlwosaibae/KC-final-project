3b9586ea93221b8bd08f9924632c6aedb83d5b65
DanaAlwosaibae <142634164+DanaAlwosaibae@users.noreply.github.com>
2023-09-13 15:44:33 +0300

    Update final.py

--- final.py
+++ final.py
@@ -1,175 +1,150 @@
-makeup_brand=['sephora','MAC','makeup forever']
- 
-skincare_brand=['cearaVe','Olay','cetaphill']
+#Products and options
+sephora = {
+    'name': 'rose gold',
+    'type': 'eyeshadow palette',
+    'price': 25.500
+}
 
-clothes_brand=['Gucci','Zara','Nike']
-
-
-sephora=[{  
-    'name':'rose gold',
-    'type':'eyeshadow plaette',
-    'price':25.500
-
-         }]
-
-Mac=[{
-    'name':'MAC matte Lipstick',
+Mac = {
+    'name': 'MAC matte Lipstick',
     'type': 'lipstick',
+    'price': 7.000
+}
+
+makeup_forever = {
+    'name': 'foundation 118',
+    'type': 'invisible cover stick',
+    'price': 15.000
+
+}
+
+cearaVe = {
+    'name': 'cearaVe SA smoothing cleanser',
+    'type': 'for dry skin',
+    'price': 5.000
+}
+
+Olay = {
+    'name': 'olay regenerist',
+    'type': 'regenerist',
+    'price': 12.520
+}
+
+cetaphill = {
+    'name': 'cetaphill moisturizing cream',
+    'type': 'moisturizing cream',
+    'price': 2.435
+}
+Gucci = {
+    'name':'linen and cotton-blend jacqard wide-leg pants',
+    'color':'baige',
+    'price':'460.000'
+
+}
+zara = {
+    'name':'short sleeve sweatshirt',
     'price':7.000
-     }]
-makeup_forever=[{
-     'name':'foundation 118',
-     'type': 'invisible cover stick',
-     'price':15.000
-                 }]
-
-def makeup():
-    makeup_choose=input('choose one otion sephora / MAC /makeup forever : ')
-    if makeup == ('sephora'):
-        return (sephora)
-    elif makeup == ('MAC'):
-        return (Mac)
-    elif makeup == ('makeup forever'):
-        return (makeup_forever)
-    else:
-        return ('erorr')
-    
-cearaVe={
-    'name':'cearaVe SA smothing cleanser',
-    'type':'for dry skin',
-    'price':5.000
-                }
-
-Olay={
-    'name':'olay regenerist',
-    'typr':'regenerist',
-    'price':12.520
+}
+Nike = {
+    'name':'nike ari max',
+    'price': 69.000
 }
 
-cetaphill={
-    'name':'cetaphill mositurizing cream',
-    'type':'mositurizing cream',
-    'price':2.435
-}
-def skincare():
-  skincare=input('choose one otion cearaVe/Olay/cetaphill : ')
-  if skincare == ('cearaVe'):
-      return (cearaVe)
-  if skincare == ('Olay'):
-      return (Olay)
-  if skincare == ('cetaphill'):
-      return (cetaphill)
- 
+# brands
+makeup_brand = ['sephora', 'MAC', 'makeup forever']
+skincare_brand = ['cearaVe', 'Olay', 'cetaphill']
+clothes_brand = ['Gucci', 'Zara', 'Nike']
 
-
-def choose1(): 
-    choos1 = input('choose one otion makeup / skincare /clothes : ')
-    if choos1 == ('mkeupe'):
-        return(makeup_brand)
-            
-    elif choos1 == ('skincare'):
-         return(skincare_brand)
-
-    elif choos1 == ('clothes'):
-         return(clothes_brand)
-    
-    else:
-        return('error')  
-
-# ==========================
-
-skincare_brand=['cearaVe','Olay','cetaphill']
-clothes_brand=['Versace','Zara','Nike']
-cars_brand=['BMW','Mercedes-benz','Toyota']
-
-def choose2():
-     choos2=input('choose one otion skincare /clothes / cars : ')
-     if choos2 == ('skincare'):
-         return(skincare_brand)
-
-     elif choos2 == ('clothes'):
-         return(clothes_brand)
-
-     elif choos2 == ('cars'):
-          return(cars_brand)
-     else:
-           return('error')
-
-# ==========================
-
-toys_brand=['Lego','toysrus']
-
-skincare_brand2=['Johansons baby', 'QV','seba med']
-
-def choose3():
-    choos3=input('choose one otion \toys skincare \clothes : ')
-    if choos3 ==('Lego'):
-        return(toys_brand)
-
-    elif choos3== ('skincare'):
-        return(skincare_brand2)
-
-    elif choos3 == ('clothesc'):
-        return(clothes_brand)
-    else:
-        return('error')
-
-print('hi!welcome to the world store')
-
-print('we need your information to see the products')
-
-name=input('enter your name : ')
-
-print('are you male ?/ female ?/ kid? : ')
-gender=input('enter your gander ')
-
-def gender_product(gender1):
-
-    if gender1 == ('female'):
-        product_for_female=['makeup','skincare','clothes']
+# gender categories
+def gender_product(gender):
+    if gender == 'female':
+        product_for_female = ['makeup', 'skincare', 'clothes']
         print(product_for_female)
-        return (choose1())
-
-    elif gender1 == ('male'):
-        product_for_male=['skincare','clothes','cars']
+        return choose1()
+    elif gender == 'male':
+        product_for_male = ['skincare', 'clothes', 'cars']
         print(product_for_male)
-        return (choose2())
-        
-
-    elif gender1 == ('kid'):
-        product_for_kid=['toys','clothes','skincare']
-        print(product_for_kid)
-        return (choose3())
-
+        return choose2()
     else:
-       return('erorr')
+        return 'error'
+#clothes
 
+
+#makeup
+def makeup():
+    makeup_choose = input('choose one option sephora / MAC / makeup forever : ')
+    if makeup_choose == 'sephora':
+        return sephora
+    elif makeup_choose == 'MAC':
+        return Mac
+    elif makeup_choose == 'makeup forever':
+        return makeup_forever
+    else:
+        return 'error'
+
+#skincare
+def skincare():
+    skincare_choose = input('choose one option cearaVe / Olay / cetaphill : ')
+    if skincare_choose == 'cearaVe':
+        return cearaVe
+    elif skincare_choose == 'Olay':
+        return Olay
+    elif skincare_choose == 'cetaphill':
+        return cetaphill
+    else:
+        return 'error'
+
+# female options
+def choose1():
+    choos1 = input('choose one option makeup / skincare / clothes : ')
+    if choos1 == 'makeup':
+        return makeup()
+    elif choos1 == 'skincare':
+        return skincare()
+    elif choos1 == 'clothes':
+        return clothes_brand
+    else:
+        return 'error'
+#==========================
+BMW = {
+    'name':'BMW 230i coupe M sport',
+    'price':18.900
+}
+Mercedes_benz = {
+    'name':'Mercedes-benz GLB',
+    'price':17.100
+}
+Toyota = {
+    'name':'toyota camry',
+    'price':10.000
+}
+
+
+
+# male brands
+skincare_brand_male = ['cearaVe', 'Olay', 'cetaphill']
+clothes_brand_male = ['Gucci', 'Zara', 'Nike']
+cars_brand = ['BMW', 'Mercedes-benz', 'Toyota']
+
+#male options
+def choose2():
+    choos2 = input('choose one option skincare / clothes / cars : ')
+    if choos2 == 'skincare':
+        return skincare_brand_male
+    elif choos2 == 'clothes':
+        return clothes_brand_male
+    elif choos2 == 'cars':
+        return cars_brand
+    else:
+        return 'error'
+
+
+# intro message
+print('Hi! Welcome to the World Store')
+name = input('Enter your name: ')
+print('Are you male? / female? : ')
+gender = input('Enter your gender: ')
 print(gender_product(gender))
-
-makeup_product=['rose gold','MAC Lipstick','foundation 118']
-ephora=('rose gold')
-Mac=('MAC Lipstick')
-makeup_forever=('foundation 118')
+user_country = input('Enter your country: ')
 
 
-cearaVe=('motisturising lotion')
-Olay=('olay natural white')
-
-
-skincare_product=['motisturising lotion','olay natural white with day dream',]
-clothes_product=['Gucci pelted cotton-gabardine trench coat','adelide midi ress in black crepe','essential cropped jacket']
-
-
-toys_product=['']
-
-
-lego=('lego harry potter')
-
-toysrus=('bingo game') 
-
-  
-         
-makeup_product=['rose gold','MAC Lipstick','foundation 118']
-skincare_product=['motisturising lotion','olay natural white with day dream']
-country=['Kuwait','Saudi Arabia','Oman','Qatar','Bahrain','U.A.U']
-user_country=input('entr your country')
